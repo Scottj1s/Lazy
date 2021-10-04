9c665be8cfc1d25e92b48c0d133bd4ef29fe045d
Scott Jones <sjones@microsoft.com>
2021-10-04 13:16:35 -0700

    Add project files.

---
+++ Lazy/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+
+namespace LazyTest
+{
+    class MyLazy<T> where T : class
+    {
+        private T _value = default;
+        private Func<T> _factory;
+
+        public MyLazy(Func<T> factory)
+        {
+            _factory = factory;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (_value == default)
+                {
+                    Interlocked.CompareExchange(ref _value, _factory(), default);
+                }
+                return _value;
+            }
+        }
+    }
+
+    class Boolean
+    {
+        public Boolean() => BoolProp = false;
+        public bool BoolProp { get; set; }
+    }
+
+    class Program
+    {
+        static Boolean EagerBool = default;
+        static readonly Lazy<Boolean> LazyBool = new Lazy<Boolean>(() => new Boolean());
+        static readonly MyLazy<Boolean> MyLazyBool = new MyLazy<Boolean>(() => new Boolean());
+
+        static Boolean MyBoolProp
+        {
+            get
+            {
+                if (EagerBool == default)
+                {
+                    Interlocked.CompareExchange(ref EagerBool, new Boolean(), default);
+                }
+                return EagerBool;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            {
+                var timer = new System.Diagnostics.Stopwatch();
+                timer.Start();
+
+                for (int i = 0; i < 10_000_000; i++)
+                {
+                    var _ = MyBoolProp.BoolProp;
+                }
+
+                timer.Stop();
+                System.Console.WriteLine("MyBoolProp: {0} ms", timer.ElapsedMilliseconds);
+            }
+
+            {
+                var timer = new System.Diagnostics.Stopwatch();
+                timer.Start();
+
+                for (int i = 0; i < 10_000_000; i++)
+                {
+                    var _ = MyLazyBool.Value.BoolProp;
+                }
+
+                timer.Stop();
+                System.Console.WriteLine("MyLazyBool: {0} ms", timer.ElapsedMilliseconds);
+            }
+
+            {
+                var timer = new System.Diagnostics.Stopwatch();
+                timer.Start();
+
+                for (int i = 0; i < 10_000_000; i++)
+                {
+                    var _ = LazyBool.Value.BoolProp;
+                }
+
+                timer.Stop();
+                System.Console.WriteLine("LazyBool: {0} ms", timer.ElapsedMilliseconds);
+            }
+
+            {
+                var timer = new System.Diagnostics.Stopwatch();
+                timer.Start();
+
+                for (int i = 0; i < 10_000_000; i++)
+                {
+                    var _ = EagerBool.BoolProp;
+                }
+
+                timer.Stop();
+                System.Console.WriteLine("EagerBool: {0} ms", timer.ElapsedMilliseconds);
+            }
+        }
+    }
+}
